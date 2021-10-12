f19f2dda591c07ec142ca0e5a1c273064fdbdc55
YoungChanShin <chan8149@naver.com>
2021-10-12 17:43:27 +0900

    주석 수정

--- leetcode/75/final.py
+++ leetcode/75/final.py
@@ -1,4 +1,5 @@
-# window에서 관리하는 데이터 : 새로 유입되는
+# window에서 관리하는 데이터 : Dequeue until it encounters a value greater than the new data in the order of the most recently inserted data.
+# 최근 삽입한 데이터 순서로 새 데이터보다 더 큰 값이 나올 때까지 큐에서 삭제
 import collections
 
 
@@ -6,14 +7,17 @@ def maxSlidingWindow(nums, k: int):
     if k == 1:
         return nums
 
-    answer = []
+    answer = []  # return
     window = collections.deque()
 
     for idx, val in enumerate(nums):
         in_idx = idx
         out_idx = idx - k
+        # 윈도우 밖의 데이터 삭제
         if window and window[0] == out_idx:
             window.popleft()
+
+        # 최근에 삽입된 데이터의 순서로 새 데이터보다 큰 값을 만날 때까지 큐에서 빼기 - 결과적으로 내림차순
         while window and nums[window[-1]] < nums[in_idx]:
             window.pop()
         window.append(in_idx)
@@ -27,6 +31,3 @@ def maxSlidingWindow(nums, k: int):
 nums = [1, 3, -1, -3, 5, 3, 6, 7]
 k = 3
 print(maxSlidingWindow(nums, k))
-
-
-# 이제 이 풀이는 시간제한에 걸려서 통과되지 않는다.
